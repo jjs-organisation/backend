1dd42455dc3b5e793c394f4b135dc707360bf417
Ip <exestman@gmail.com>
2023-11-17 05:00:59 +0300

    -v -https

--- index.js
+++ index.js
@@ -5,31 +5,51 @@ const express = require("express"),
     router_users = require('./api/users'),
     router_projects = require('./api/projects'),
     router_files = require('./api/files'),
-    port = 3000,
+    port = 3450,
+    sport = 3451,
+    http = require("http"),
     cors = require('cors'),
     path = require("path"),
-    bodyparser = require("body-parser");
+    fs = require("fs"),
+    https = require("https"),
+    key = fs.readFileSync(__dirname + '/sert/selfsigned.key'),
+    cert = fs.readFileSync(__dirname + '/sert/selfsigned.crt'),
+    bodyparser = require("body-parser"),
+    options = {
+        key: key,
+        cert: cert
+    };
 
 app.use('/projects/', router_projects);
 app.use('/users/', router_users);
 app.use('/files/', router_files);
-
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json());
 app.use(cors({
     origin: '*'
 }));
-
 app.use((req, res, next) => {
     res.set('Cache-Control', 'no-store')
     next();
 })
 
-app.listen(port,() => {
+app.get('/po',() => {
     console.log({ message: 'listen' })
 });
 
-app.get('/po',() => {
-    console.log({ message: 'listen' })
+app.get('/', (req, res) => {
+    res.send('Now using https..');
+});
+
+const server_http = http.createServer(app);
+const server_https = https.createServer(options, app);
+
+
+server_http.listen(port, () => {
+    console.log(`server running at https://95.163.233.114:${sport}`)
+});
+
+server_https.listen(sport, () => {
+    console.log(`server running at https://95.163.233.114:${sport}`)
 });
